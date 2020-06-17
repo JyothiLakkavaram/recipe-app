9765c34f9f518dd2fd753dbd18d8cb8c8b7fdca6
Jyothi <jyothi.lakkavaram@gmail.com>
2020-06-18 01:33:35 +0530

    Recipe-app

---
+++ precache-manifest.51872647a799d0a76dbc4ae373ad668f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "024c3ed41a8dfb409bcc202ef3f5e97f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc24e245a8162b67f7b6",
+    "url": "/static/css/main.8229c49b.chunk.css"
+  },
+  {
+    "revision": "fccc96eea13c59befb4c",
+    "url": "/static/js/2.549303be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.549303be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc24e245a8162b67f7b6",
+    "url": "/static/js/main.faac31b1.chunk.js"
+  },
+  {
+    "revision": "4819ed60826abbb60a97",
+    "url": "/static/js/runtime-main.2866e11d.js"
+  }
+]);
